6eba4340d57e9dee58ef8447223307b763ffdb08
Guilherme Coelho <gui_coelho@hotmail.com>
2017-01-19 09:28:50 -0200

    Alterações em Decoder.cpp

--- MTSP_BRKGA/Decoder.cpp
+++ MTSP_BRKGA/Decoder.cpp
@@ -6,6 +6,9 @@ Decoder::Decoder(signed char** mat,
                  int rows,
                  int coloumns) {
     
+    this->cols = coloumns;
+    this->rows = rows;
+    
     memcpy(matriz, mat, sizeof(signed char**) * rows * coloumns);
     
     std::cout << "Matriz Decoder" << std::endl;
@@ -16,18 +19,67 @@ Decoder::Decoder(signed char** mat,
         }
         std::cout << std::endl;
     }
+    std::cout << std::endl;
+
 }
 
-Decoder::~Decoder() { }
+Decoder::~Decoder() {
+    
+    for (int i = 0;  i < rows; i++) {
+        matriz[i] = NULL;
+    }
+    
+    matriz = NULL;
+    
+    rows = 0;
+    cols = 0;
+}
 
-double Decoder::decode(const std::vector< double >& chromosome) const  {
+void printToolsVet(std::vector<int> vet){
+    
+    for (int i = 0; i < vet.size(); i++) {
+        std::cout << vet[i] << " ";
+    }
+    std::cout<<std::endl;
+    
+}
+
+std::vector<int> setUpToolsVet(std::vector<double> cromossomos){
+    
+    std::vector<int> vet;
+
+    // iterador para conseguir o maior elemento do vetor de cromossomo
+    std::vector<double>::iterator it = max_element(cromossomos.begin(),cromossomos.end());
+    
+    double maior = (double) *it;
+    
+    // preenche o vetor de ferramentas sempre com o maior elemento do vetor de cromossomo
+    while ( maior != -1) {
+        int pos = (int) distance(cromossomos.begin(),it);
+        vet.push_back(pos);
+        cromossomos[pos] = -1;
+        it = max_element(cromossomos.begin(),cromossomos.end());
+        maior = (double) *it;
+    }
+
+    return vet;
+}
+
+double Decoder::decode( std::vector< double >& chromosome) const  {
     
 	double myFitness = 0.0;
     
-    for (int i = 0; i < chromosome.size(); i++) {
-        std::cout<< chromosome[i]<<" ";
+    if ( !chromosome.empty() ) {
+        
+        // Vetor de Ferramentas
+        std::vector<int> toolsVet = setUpToolsVet(chromosome);
+        
+        printToolsVet(toolsVet);
+
     }
-    std::cout<<std::endl;
+    
 	// Return the fitness:
 	return myFitness;
 }
+
+
--- MTSP_BRKGA/Decoder.h
+++ MTSP_BRKGA/Decoder.h
@@ -6,16 +6,16 @@
 class Decoder {
 public:
     
-    Decoder(signed char**,int,int);	// Constructor - Matrix
-    
+    Decoder(signed char**,int,int);	// Constructor - Matrix    
 	~Decoder();	// Destructor
         
-    // Public Methods
     
 	// Decode a chromosome, returning its fitness as a double-precision floating point:
-	double decode(const std::vector< double >& chromosome) const;
-
+	double decode( std::vector< double >& chromosome) const;
+    
 private:
     const signed char** matriz;
+    int rows;
+    int cols;
 };
 
